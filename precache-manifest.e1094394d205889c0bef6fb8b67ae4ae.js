cca34627ed42d774e8e754528c9c35f2a278e523
atlasmoth <ebuka422@gmail.com>
2020-02-27 21:27:02 +0100

    Updates

---
+++ precache-manifest.e1094394d205889c0bef6fb8b67ae4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d73cd5a0277997cff58796929dca2f6",
+    "url": "/CHUKWUIEBUKA-OSUJI-Shecluded-Frontend-Task-2020/index.html"
+  },
+  {
+    "revision": "bf457d61052fd6eda19d",
+    "url": "/CHUKWUIEBUKA-OSUJI-Shecluded-Frontend-Task-2020/static/css/main.d7357595.chunk.css"
+  },
+  {
+    "revision": "f0a565f072c52d382aa3",
+    "url": "/CHUKWUIEBUKA-OSUJI-Shecluded-Frontend-Task-2020/static/js/2.ba0a0bd7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/CHUKWUIEBUKA-OSUJI-Shecluded-Frontend-Task-2020/static/js/2.ba0a0bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf457d61052fd6eda19d",
+    "url": "/CHUKWUIEBUKA-OSUJI-Shecluded-Frontend-Task-2020/static/js/main.ccf48220.chunk.js"
+  },
+  {
+    "revision": "7f6b952db5a15f9448bd",
+    "url": "/CHUKWUIEBUKA-OSUJI-Shecluded-Frontend-Task-2020/static/js/runtime-main.cbb2b3ab.js"
+  }
+]);
